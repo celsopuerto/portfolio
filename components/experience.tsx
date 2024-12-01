623f1f53e84d95d6598cc0ae371233d5858d1fbc
celsopuerto <isoygwapo84@gmail.com>
2024-12-01 13:28:28 +0800

    changed img to Image

--- components/experience.tsx
+++ components/experience.tsx
@@ -9,34 +9,35 @@ import { useSectionInView } from '@/lib/hooks';
 import { useTheme } from '@/context/theme-context';
 import { FaMapMarkerAlt } from 'react-icons/fa';
 import { motion } from 'framer-motion';
+import Image from 'next/image'; // Import Image from next/image
 
 export default function Experience() {
     const { ref } = useSectionInView("Experience", 0.5);
     const { theme } = useTheme();
 
   return (
-  <section ref={ref} id="experience" className="scroll-mt-28 mb-28 sm:mb-40">
-    <SectionHeading>My experience</SectionHeading>
-    <VerticalTimeline lineColor=''>
+    <section ref={ref} id="experience" className="scroll-mt-28 mb-28 sm:mb-40">
+      <SectionHeading>My experience</SectionHeading>
+      <VerticalTimeline lineColor=''>
         {experiencesData.map((item, index) => (
             <React.Fragment key={index}>
                 <VerticalTimelineElement
-                contentStyle={{
+                  contentStyle={{
                     background: theme === 'light' ?"#f3f4f6" : 'rgba(255, 255, 255, 0.05)',
                     boxShadow: "none",
                     border: "1px solid rgba(0, 0, 0, 0.05)",
                     textAlign: "left",
                     padding: "1.3rem 2rem",
-                }}
-                contentArrowStyle={{
-                     borderRight: theme === 'light' ? "0.4rem solid #9ca3af" : '0.4rem solid rgba(255, 255, 255, 0.5)',
-                }}
-                date={item.date}
-                icon={item.icon}
-                iconStyle={{
+                  }}
+                  contentArrowStyle={{
+                    borderRight: theme === 'light' ? "0.4rem solid #9ca3af" : '0.4rem solid rgba(255, 255, 255, 0.5)',
+                  }}
+                  date={item.date}
+                  icon={item.icon}
+                  iconStyle={{
                     background: theme === 'light' ? "white" : 'rgba(255, 255, 255, 0.15)',
                     fontSize: "1.5rem",
-                }}
+                  }}
                 >
                     <h3 className="font-semibold capitalize">{item.title}</h3>
                     <p className="font-normal !mt-0">
@@ -47,42 +48,40 @@ export default function Experience() {
                 </VerticalTimelineElement>
             </React.Fragment>
         ))}
-    </VerticalTimeline>
+      </VerticalTimeline>
 
-
-        {/* CERTIFICATES */}
-
-
-    <div className="certifications-wall">
-            
-    {certificationsData.map((cert, index) => (
-        <motion.div
-        key={index}
-        className="certification-card"
-        whileHover={{ scale: 1.1 }} // Zoom effect on hover
-        transition={{ type: "spring", stiffness: 300 }} // Smooth transition
-        >
-        <div className="certification-image">
-            {/* Display the certificate image */}
-            <img
-            src={cert.image}
-            alt={cert.name}
-            className="certification-image borderBlack"
-            />
-        </div>
-        <h3>{cert.name}</h3>
-        <p>{cert.description}</p>
-        {cert.credentialLink !== "" ? (
-            <a href={cert.credentialLink} target="_blank" rel="noopener noreferrer">
-            View Credential
-            </a>
-        ) : (
-            <a href=""></a>
-        )}
-        </motion.div>
-    ))}
-    </div>
-  </section>
-  
+      {/* CERTIFICATES */}
+      <div className="certifications-wall mt-16">
+        {certificationsData.map((cert, index) => (
+            <motion.div
+              key={index}
+              className="certification-card"
+              whileHover={{ scale: 1.1 }} // Zoom effect on hover
+              transition={{ type: "spring", stiffness: 300 }} // Smooth transition
+            >
+                <div className="certification-image">
+                    {/* Display the certificate image using Next.js Image */}
+                    <Image
+                      src={cert.image}
+                      alt={cert.name}
+                      className="certification-image border-2 border-black rounded-lg"
+                      width={500}  // Adjust width as needed
+                      height={300} // Adjust height as needed
+                      objectFit="contain" // Maintains image aspect ratio
+                    />
+                </div>
+                <h3>{cert.name}</h3>
+                <p>{cert.description}</p>
+                {cert.credentialLink !== "" ? (
+                    <a href={cert.credentialLink} target="_blank" rel="noopener noreferrer">
+                      View Credential
+                    </a>
+                ) : (
+                    <a href="#"></a>
+                )}
+            </motion.div>
+        ))}
+      </div>
+    </section>
   );
 }
